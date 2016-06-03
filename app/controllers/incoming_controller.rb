f660880c21b72f6bd26a7437e615a6d25bf01d05
Bennet11 <bennetveloya@gmail.com>
2016-06-03 13:56:42 -0700

    incoming_email test

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,28 +1,37 @@
 class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
-
+  skip_before_action :authenticate_user!
+  
   def create
+    # Find the user by using params[:sender]
+    # Find the topic by using params[:subject]
+    # Assign the url to a variable after retreiving it from params["body-plain"]
 
-     # Find the user by using params[:sender]
-     @user = User.find_by(email: params[:sender])
-     # Find the topic by using params[:subject]
-     topic = Topic.find_or_create_by(name: params[:subject])
-     # Assign the url to a variable after retreiving it from params["body-plain"]
-     @bookmark.url = params["body-plain"]
-     # Check if user is nil, if so, create and save a new user
-     if user.nil?
-       redirect_to new_user_path
-     end
+
+    @user = User.find_by_email(params[:sender])
+    @topic = Topic.find_by_title(params[:subject])
+    @url = params["body-plain"]
+
+
+    # Check if user is nil, if so, create and save a new user
+    if @user.nil?
+      @user = User.new(
+        email:                 params[:sender],
+        password:              params[:sender],
+        password_confirmation: params[:sender]
+      )
+      @user.save!
+    end
+
      # Check if the topic is nil, if so, create and save a new topic
-     if topic.nil?
-       redirect_to new_topic_path
-     end
-     # Now that you're sure you have a valid user and topic, build and save a new bookmark
-     @bookmark = Bookmark.new
-     @bookmark.topic_id = @topic.id
-     @bookmark.url = @url
-     @bookmark.save!
+    if @topic.nil?
+      @topic = @user.topics.build(title: params[:subject])
+      @topic.save!
+    end
 
-     head 200
+     # Now that you're sure you have a valid user and topic, build and save a new bookmark
+    @bookmark = @topic.bookmarks.build(url: @url)
+    @bookmark.save
+    head 200
   end
 end
